04bd89077cd3e62498039dceb77149f5c091132e
Florian <florian.escude@gmail.com>
2020-11-30 20:07:23 +0100

    [ADD] - Adding alert message for login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,12 @@
 import React, {useState} from "react";
-import {Button, Card, Form} from "react-bootstrap"
-import {useAuth} from "../../contexts/AuthContext";
+import {Alert, Button, Card, Form} from "react-bootstrap"
+import AuthService from "../../services/AuthService";
 
 const Login = () => {
 
-    const auth = useAuth();
-
-    const [state, setState] = useState({
-        email: '',
-        password: '',
-    });
+    const [show, setShow] = useState(false);
+    const [error, setError] = useState(null);
+    const [state, setState] = useState({email: '', password: ''});
 
     const handleChange = (event) => {
         const {name, value} = event.target;
@@ -24,22 +21,52 @@ const Login = () => {
         event.preventDefault();
 
         const {email, password} = state
-        auth.authLogin(email, password);
+        AuthService.login(email, password)
+            .then(response => {
+                const {userId, token} = response.data;
+                localStorage.setItem("userId", userId)
+                localStorage.setItem("token", token)
+            })
+            .catch(error => {
+                let errorStatus = error.response.status;
+                if (errorStatus === 401) {
+                    setError("Invalid credentials.")
+                }
+                if (errorStatus === 500) {
+                    setError("Internal server error.")
+                }
+                setShow(true)
+            })
+        ;
+    }
+
+    let alertDialog = null;
+    if (show) {
+        alertDialog = (
+            <Alert variant="danger" className="col-sm-12 col-md-8 mt-4 ml-auto mr-auto" onClose={() => setShow(false)}
+                   dismissible>
+                <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+                <p>{error}</p>
+            </Alert>
+        );
     }
 
     return (
-        <div className="col-sm-12 col-md-8 ml-auto mr-auto mt-4">
-            <Card>
+        <div>
+            {alertDialog}
+            <Card className="col-sm-12 col-md-8 mt-4 ml-auto mr-auto">
                 <Card.Body>
                     <h2 className="text-center">Log In</h2>
                     <Form onSubmit={handleSubmit}>
                         <Form.Group id="email">
                             <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" name="email" value={state.email} onChange={handleChange} required/>
+                            <Form.Control type="email" name="email" value={state.email} onChange={handleChange}
+                                          required/>
                         </Form.Group>
                         <Form.Group id="password">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" name="password" value={state.password} onChange={handleChange} required/>
+                            <Form.Control type="password" name="password" value={state.password} onChange={handleChange}
+                                          required/>
                         </Form.Group>
                         <Button type="submit" className="w-100 mt-2">
                             Log In
