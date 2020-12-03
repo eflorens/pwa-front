2b358ffe5e3c536b5a5d18ad49048ae02aa548ee
Thibault <thibault.hebert@epitech.eu>
2020-12-03 14:55:49 +0100

    Add base profile

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const Profile = ({ item }) => {
+  return (
+    <Container>
+      <Row>
+        <Col>1 of 2</Col>
+        <Col>2 of 2</Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Profile;
