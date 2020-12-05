a6961d64646f4f302308da8a496881ca4a7120cb
Thibault <thibault.hebert@epitech.eu>
2020-12-05 01:11:19 +0100

    WIP

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -24,6 +24,7 @@ const Toolbar = (props) => {
       };
         const data = await axios.post(`${API_URL}/img`, formData, config)
         if (data) setIsShow(!isShow)
+        window.location.reload();
       }
     
       const _handleImageChange = (e) => {
